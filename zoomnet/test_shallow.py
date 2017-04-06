c6470bda78ec70cc618ad68e6b2beef09291a4c2
Bijan Fakhri (Student) <bfakhri@en4113734l.fulton.ad.asu.edu>
2017-04-06 13:08:06 -0700

    Learning a LITTLE

--- zoomnet/test_shallow.py
+++ zoomnet/test_shallow.py
@@ -1,10 +1,3 @@
-'''Trains a simple convnet on the MNIST dataset.
-
-Gets to 99.25% test accuracy after 12 epochs
-(there is still a lot of margin for parameter tuning).
-16 seconds per epoch on a GRID K520 GPU.
-'''
-
 from __future__ import print_function
 import keras
 from keras.datasets import mnist
@@ -16,12 +9,22 @@ from vec import vec
 from keras.utils import plot_model
 
 
-batch_size = 1
-epochs = 4
+batch_size = 1 
+epochs = 20
+
+activation_fnc = 'relu'
+#activation_fnc = 'tanh'
+#activation_fnc = 'sigmoid'
 
 # the data, shuffled and split between train and test sets
 v = vec()
-(x_train, y_train), (x_test, y_test) = v.load_data()
+(x_train, y_train), (x_trainval, y_trainval), (x_test, y_test) = v.load_pure()
+# FOR DEBUGGING
+x_train = x_train[0:20, :, :, :]
+y_train = y_train[0:20, :]
+x_trainval = x_train
+y_trainval = y_train
+#x_train, y_train), (x_trainval, y_trainval), (x_test, y_test) = v.load_messy()
 img_rows = x_train.shape[2]
 img_cols = x_train.shape[1]
 
@@ -44,6 +47,7 @@ x_train /= 255
 x_test /= 255
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
+print(x_trainval.shape[0], 'trainval samples')
 print(x_test.shape[0], 'test samples')
 
 # convert class vectors to binary class matrices
@@ -51,20 +55,24 @@ print(x_test.shape[0], 'test samples')
 #y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-model.add(Conv2D(64, kernel_size=(25, 25),
-                 activation='relu',
+model.add(Conv2D(64, kernel_size=(5, 5),
+                 activation=activation_fnc,
                  input_shape=input_shape))
-model.add(MaxPooling2D(pool_size=(4, 4)))
-model.add(Conv2D(32, (5, 5), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(Conv2D(32, kernel_size=(5, 5),
+                 activation=activation_fnc,
+                 input_shape=input_shape))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Dropout(0.25))
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation=activation_fnc,
+                 input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(32, activation='relu'))
-#model.add(Dropout(0.5))
-model.add(Dense(32, activation='relu'))
-#model.add(Dropout(0.5))
+model.add(Dense(32, activation=activation_fnc))
+model.add(Dropout(0.5))
+model.add(Dense(32, activation=activation_fnc))
+model.add(Dropout(0.5))
 model.add(Dense(3, activation='linear'))
 
 model.compile(loss=keras.losses.mean_squared_error,
@@ -77,9 +85,9 @@ model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
-          validation_data=(x_test[1:30,:,:,:], y_test[1:30,:]))
+          validation_data=(x_trainval, y_trainval))
 score = model.evaluate(x_test[1:30,:,:,:], y_test[1:30,:], verbose=0)
-print(model.predict(x_test[1:30,:,:,:]))
-print(y_test[1:30])
+print(model.predict(x_test[1:20,:,:,:]))
+print(y_test[1:20])
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
--- zoomnet/vec.py
+++ zoomnet/vec.py
@@ -10,58 +10,14 @@ NUM_CHANS = 3
 VECT_DIMS = 3
 
 class vec:
-	def load_data(self, obj_type="bird"):
-		# Get handle to list of files 
-		trn_str = str(file_listdir)+str(obj_type)+str("_train.txt")
-		val_str = str(file_listdir)+str(obj_type)+str("_val.txt")
-		print("Opening dataset with type: "+str(obj_type))
-		print("Opening "+trn_str)
-		print("Opening "+val_str)
-		train_file_hdl = open(trn_str, "r")
-		val_file_hdl = open(val_str, "r")
-
-		train_list = []
-		val_list = []
-		# Put into vectors
-		for line in train_file_hdl:
-			spl = line.split()
-			if(spl[1] == '1'):
-				#print("Train Saved: " + line)
-				train_list.append(spl[0])
-		for line in val_file_hdl:
-			spl = line.split()
-			if(spl[1] == '1'):
-				#print("Val Saved: " + line)
-				val_list.append(spl[0])
-
-		print("There are : " + str(len(train_list)) + " training samples")
-		print("There are : " + str(len(val_list)) + " validation samples")
-
-		# A little silly - but we have to find the max resolution of all images for both dims
-		MAX_X = 0
-		MAX_Y = 0
-		for sample in set(val_list).union(train_list):
-			img = cv2.imread(img_listdir+sample+".jpg")
-			height, width, channels = img.shape
-			if(height > MAX_Y):
-				MAX_Y = height
-			if(width > MAX_X):
-				MAX_X = width
-
-		print("Max Image Dimensions: " + str(MAX_X) + "x" + str(MAX_Y))	
-		print("Len(train_list): " + str(len(train_list)))	
-
+	def populate_array(self, sample_list, MAX_X, MAX_Y, obj_type):
 		# Create the output arrays for Keras
-		x_train = np.zeros([len(train_list), MAX_Y, MAX_X, NUM_CHANS], dtype=np.uint8)
-		y_train = np.zeros([len(train_list), VECT_DIMS], dtype=np.float32)
-		x_test = np.zeros([len(val_list), MAX_Y, MAX_X, NUM_CHANS], dtype=np.uint8)
-		y_test = np.zeros([len(val_list), VECT_DIMS], dtype=np.float32)
-		
+		x = np.zeros([len(sample_list), MAX_Y, MAX_X, NUM_CHANS], dtype=np.uint8)
+		y = np.zeros([len(sample_list), VECT_DIMS], dtype=np.float32)
 		# Populate those arrays	
 		single_obj_count = 0
-		for sample in train_list:
+		for sample in sample_list:
 			# Load the XML doc related to the image
-			#print("Opening: " +xml_listdir+sample+".xml") 
 			tree = ET.parse(xml_listdir+sample+".xml")
 			root = tree.getroot()
 			# Get size of image
@@ -81,46 +37,137 @@ class vec:
 					y_max = int(bnd.find('ymax').text) + (MAX_Y/2-height/2)
 					cent_x = (x_max-x_min)/2 + x_min
 					cent_y = (y_max-y_min)/2 + y_min
-					vec_x = img_cent_x - cent_x
-					vec_y = img_cent_y - cent_y
-					# Is this the best way to represent zoom? 
+					vec_x = (img_cent_x - cent_x)/float(MAX_X)
+					vec_y = (img_cent_y - cent_y)/float(MAX_Y)
 					vec_z = (x_max-x_min)*(y_max-y_min)/(float(width*height))
-					#print(str(vec_x) + " " + str(vec_y) + " " + str(vec_z))
 				else:
 					vec_x = 0
 					vec_y = 0
 					vec_z = -1	
-					#print(str(vec_x) + " " + str(vec_y) + " " + str(vec_z))
 
-			if(obj_type_count == 1):
+			if(obj_type_count <= 1):
 				# Load the image 
 				img = cv2.imread(img_listdir+sample+".jpg")
 				height, width, channels = img.shape
 				#print x_train.shape
-				x_train[single_obj_count, (MAX_Y/2-height/2):(MAX_Y/2-height/2+height), (MAX_X/2-width/2):(MAX_X/2-width/2+width), :] = img
-				temp_img = x_train[single_obj_count, :, :, :]
+				x[single_obj_count, (MAX_Y/2-height/2):(MAX_Y/2-height/2+height), (MAX_X/2-width/2):(MAX_X/2-width/2+width), :] = img
+				temp_img = x[single_obj_count, :, :, :]
 				#cv2.imshow('Centered Image', temp_img)
 				#cv2.waitKey(0)
 				#cv2.destroyAllWindows()
 				# Draw the vector on the image
-				vec_img = cv2.line(x_train[single_obj_count, :, :, :], (cent_x, cent_y), (img_cent_x, img_cent_y), (255,0,0),5)	
+				#vec_img = cv2.line(x[single_obj_count, :, :, :], (cent_x, cent_y), (img_cent_x, img_cent_y), (255,0,0),5)	
 				#cv2.imshow('Centered Image', vec_img)
 				#cv2.waitKey(0)
 				#cv2.destroyAllWindows()
 				# Make the labels
-				y_train[single_obj_count, 0] = vec_x
-				y_train[single_obj_count, 1] = vec_y
-				y_train[single_obj_count, 2] = vec_z
+				y[single_obj_count, 0] = vec_x
+				y[single_obj_count, 1] = vec_y
+				y[single_obj_count, 2] = vec_z
 				single_obj_count += 1
 
 		# Trim the output array (delete empty entries)
-		mask = np.ones(len(train_list), dtype=bool)
-		mask[range(single_obj_count, len(train_list))] = False
-		x_train = x_train[mask, :, :, :]
-		y_train = y_train[mask, :]
-		print("Number of good images (1 bird): " + str(single_obj_count))
-		print("x_train size: " + str(x_train.shape))
-		print("y_train size: " + str(y_train.shape))
+		mask = np.ones(len(sample_list), dtype=bool)
+		mask[range(single_obj_count, len(sample_list))] = False
+		x = x[mask, :, :, :]
+		y = y[mask, :]
 
-		return (x_train, y_train), (x_train, y_train)
-				
+		return (x, y)
+	
+	# Train and validation are only filled with birds			
+	def load_pure(self, obj_type="bird"):
+		# Get handle to list of files 
+		trn_str = str(file_listdir)+str(obj_type)+str("_train.txt")
+		trnval_str = str(file_listdir)+str(obj_type)+str("_trainval.txt")
+		val_str = str(file_listdir)+str(obj_type)+str("_val.txt")
+		print("Opening dataset with type: "+str(obj_type))
+		print("Opening "+trn_str)
+		print("Opening "+trnval_str)
+		print("Opening "+val_str)
+		train_file_hdl = open(trn_str, "r")
+		trainval_file_hdl = open(trnval_str, "r")
+		val_file_hdl = open(val_str, "r")
+
+		train_list = []
+		trainval_list = []
+		val_list = []
+		# Put into vectors
+		for line in train_file_hdl:
+			spl = line.split()
+			if(spl[1] == '1'):
+				train_list.append(spl[0])
+		for line in trainval_file_hdl:
+			spl = line.split()
+			if(spl[1] == '1'):
+				trainval_list.append(spl[0])
+		for line in val_file_hdl:
+			spl = line.split()
+			if(spl[1] == '1'):
+				val_list.append(spl[0])
+
+		print("There are : " + str(len(train_list)) + " training samples")
+		print("There are : " + str(len(trainval_list)) + " training validation samples")
+		print("There are : " + str(len(val_list)) + " validation samples")
+
+		# A little silly - but we have to find the max resolution of all images for both dims
+		MAX_X = 0
+		MAX_Y = 0
+		for sample in set(val_list).union(train_list):
+			img = cv2.imread(img_listdir+sample+".jpg")
+			height, width, channels = img.shape
+			if(height > MAX_Y):
+				MAX_Y = height
+			if(width > MAX_X):
+				MAX_X = width
+
+		return self.populate_array(train_list, MAX_X, MAX_Y, obj_type), self.populate_array(trainval_list, MAX_X, MAX_Y, obj_type), self.populate_array(val_list, MAX_X, MAX_Y, obj_type)
+	
+	# Training array has samples with birds and without birds present
+	def load_messy(self, obj_type="bird"):
+		# Get handle to list of files 
+		trn_str = str(file_listdir)+str(obj_type)+str("_train.txt")
+		trnval_str = str(file_listdir)+str(obj_type)+str("_trainval.txt")
+		val_str = str(file_listdir)+str(obj_type)+str("_val.txt")
+		print("Opening dataset with type: "+str(obj_type))
+		print("Opening "+trn_str)
+		print("Opening "+trnval_str)
+		print("Opening "+val_str)
+		train_file_hdl = open(trn_str, "r")
+		trainval_file_hdl = open(trnval_str, "r")
+		val_file_hdl = open(val_str, "r")
+
+		train_list = []
+		trainval_list = []
+		val_list = []
+		# Put into vectors
+		for line in train_file_hdl:
+			spl = line.split()
+			#if(spl[1] == '1'):
+			train_list.append(spl[0])
+		for line in trainval_file_hdl:
+			spl = line.split()
+			if(spl[1] == '1'):
+				trainval_list.append(spl[0])
+		for line in val_file_hdl:
+			spl = line.split()
+			if(spl[1] == '1'):
+				val_list.append(spl[0])
+
+		print("There are : " + str(len(train_list)) + " training samples")
+		print("There are : " + str(len(trainval_list)) + " training validation samples")
+		print("There are : " + str(len(val_list)) + " validation samples")
+
+		# A little silly - but we have to find the max resolution of all images for both dims
+		MAX_X = 0
+		MAX_Y = 0
+		for sample in set(val_list).union(train_list):
+			img = cv2.imread(img_listdir+sample+".jpg")
+			height, width, channels = img.shape
+			if(height > MAX_Y):
+				MAX_Y = height
+			if(width > MAX_X):
+				MAX_X = width
+
+		return self.populate_array(train_list, MAX_X, MAX_Y, obj_type), self.populate_array(trainval_list, MAX_X, MAX_Y, obj_type), self.populate_array(val_list, MAX_X, MAX_Y, obj_type)
+	
+
