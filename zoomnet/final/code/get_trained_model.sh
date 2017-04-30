d03b8a3659c3ebef297ab92e57c162fb91d0dce2
Bijan Fakhri (Student) <bfakhri@en4113734l.fulton.ad.asu.edu>
2017-04-29 19:38:55 -0700

    Added auto DL for pre-trained model

---
+++ zoomnet/final/code/get_trained_model.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget https://github.com/bfakhri/localvec/blob/master/zoomnet/model_weights.h5?raw=true -O model_weights.h5
+wget https://raw.githubusercontent.com/bfakhri/localvec/master/zoomnet/model_params.json -O model_params.json
